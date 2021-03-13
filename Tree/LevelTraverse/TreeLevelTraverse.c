bcfbedd75723afd256343bc7d3ad429b0b5388fe
youngkyunIm <yee0230@naver.com>
2021-03-13 23:03:14 +0900

    _

--- Tree/LevelTraverse/TreeLevelTraverse.c
+++ Tree/LevelTraverse/TreeLevelTraverse.c
@@ -5,8 +5,11 @@ typedef struct *node
     int data;
     struct *node left;
     struct *node right;
+	struct *node date;
 
     struct *node Queue;
-}node;#include<stdio.h>
+}node;
 
-
+node *insert(int value)
+{
+	
