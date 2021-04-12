eec843056855dc551e721e4f16325712397b8b40
youngkyunIm <yee0230@naver.com>
2021-04-12 20:33:03 +0900

    CBtree

--- Tree/CBtrees/CBtree.c
+++ Tree/CBtrees/CBtree.c
@@ -1,3 +1,29 @@
+//Complete binary tree
 #include<stdio.h>
 
+typedef struct node
+{
+	int data;
+	struct node* right;
+	struct node* left;
+}node;
 
+node* root;
+
+node* insert(node* root,int value)
+{
+	if (root == NULL)
+	{
+		root -> data = value;
+
+	}
+}
+
+delete()
+{
+}
+
+int main()
+{
+	
+}
