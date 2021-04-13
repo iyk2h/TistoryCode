9e4b567c52245ed87de16e1f37c8874d552a8f03
youngkyunIm <yee0230@naver.com>
2021-04-13 11:57:07 +0900

    ing..

--- Tree/CBtrees/CBtree.c
+++ Tree/CBtrees/CBtree.c
@@ -1,5 +1,6 @@
 //Complete binary tree
 #include<stdio.h>
+#include<stdlib.h>
 
 typedef struct node
 {
@@ -8,22 +9,34 @@ typedef struct node
 	struct node* left;
 }node;
 
-node* root;
+node* root = NULL;
 
 node* insert(node* root,int value)
 {
 	if (root == NULL)
 	{
+		root = (node*)malloc(sizeof(node));
 		root -> data = value;
-
+		root -> right = root -> left = NULL;
 	}
+if 
+	return root;
 }
 
-delete()
+node* delete(node* root)
 {
+	if(root == NULL)
+	{
+		printf("don't have moer node");
+		return root=NULL;
+	}
 }
 
 int main()
 {
-	
+	delete(root);
+	root = insert(root, 5);
+	printf("%d",root->data);
+	delete(root);
+	printf("%d",root->data);
 }
